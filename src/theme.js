68defe73cc1a6b3c0acdd4f328bde8db1d96e42d
Dan Nguyen <nguyenphungmaidan@gmail.com>
2024-11-27 16:40:08 +0700

    install simple theme

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { cyan, orange, teal } from "@mui/material/colors";
+
+const theme = extendTheme({
+  trello: {
+    appBarHeight: "48px",
+    boardBarHeight: "58px",
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: orange,
+      },
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
+    },
+  },
+  // ...other properties
+});
+export default theme;
