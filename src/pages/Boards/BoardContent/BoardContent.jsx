aa5cca7b44236c3af477106bc0b86ad41150092a
Dan Nguyen <nguyenphungmaidan@gmail.com>
2024-12-03 08:17:53 +0700

    code boardContent part 2

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -2,8 +2,6 @@ import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 
 function BoardContent() {
-
-
   return (
     <Box sx={{
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
